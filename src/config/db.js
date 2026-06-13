c7473b8b6f2ffe10e591969e01baddead7cce09d
JuanBabarro <juanbabarro99@gmail.com>
2026-06-12 21:55:36 -0300

    Nueva Version

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const conexion = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'book_app',
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST || process.env.MYSQL_ADDON_HOST || 'localhost',
+    user: process.env.DB_USER || process.env.MYSQL_ADDON_USER || 'root',
+    password: process.env.DB_PASSWORD || process.env.MYSQL_ADDON_PASSWORD || '',
+    database: process.env.DB_NAME || process.env.MYSQL_ADDON_DB || 'book_app',
+    port: process.env.DB_PORT || process.env.MYSQL_ADDON_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
